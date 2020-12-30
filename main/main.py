af3b0d033943e8917574191cb31774276c8a0796
Anthony Herrera <anthony.herrera@pioneer.com>
2020-12-30 14:04:48 -0600

    added requirements file and updated code

--- main/main.py
+++ main/main.py
@@ -1,6 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import nnfs
+import matplotlib.pyplot as plt
 from nnfs.datasets import vertical_data
 
 nnfs.init()
@@ -34,9 +34,9 @@ class Activation_RelU:
         Activates the nuerons
         """
         pass
-    
+
     def forward(self, inputs):
-        self.inputs = inputs 
+        self.inputs = inputs
         self.output = np.maximum(0, inputs)
 
     def backwards(self, dvalues):
@@ -51,17 +51,33 @@ class Activation_Softmax:
         Cleans up the output values so they are uniform without throwing out negative values.
         """
         pass
-    
+
     def forward(self, inputs):
+        # Keep for later
+        self.inputs = inputs
+
+        # Reduce all by largest value, take log, normalize values
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        probabilities = exp_values / np.sum(exp_values, axis=1 , keepdims=True)
         self.output = probabilities
+
+    def backward(self, dvalues):
+
+        # Create uninitialized array
+        self.dinputs = np.empty_like(dvalues)
+
+        # Enumerate outputs and gradients
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+            # Flatten
+            single_output = single_output.reshape(-1,1)
+            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
 
 
 class Loss:
     def __init__(self):
         """
-        Calculates the data aand regularization losses given model output 
+        Calculates the data aand regularization losses given model output
         and ground truth values. This is a common class that will be inherited by later loss classes
         to build upon
         """
@@ -74,88 +90,113 @@ class Loss:
         sample_losses = self.forward(output, y)
         data_loss = np.mean(sample_losses)
         return data_loss
-    
+
 class Loss_CategoricalCrossEntropy(Loss):
+    def __init__(self):
+        pass
+
+
     def forward(self, y_pred, y_true):
+        """
+        Run values forward to calculate loss
+        Args:
+            y_pred: predicated values
+            y_true: actual values
+        """
         samples = len(y_pred)
         # clip to prevent log(0) error. Both sides to prevent value biasing
         y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
-        
+
         # Checking for categorical labels vs one-hot encoded
         if len(y_true.shape) == 1:
             correct_confidences = y_pred_clipped[
             range(samples),
             y_true
             ]
-            # Mask values - only for one-hot encoded labels
+
         elif len(y_true.shape) == 2:
+            # Mask values - only for one-hot encoded labels
             correct_confidences = np.sum(
             y_pred_clipped*y_true,
             axis=1
             )
-        
-        negative_log = -np.log(correct_confidences)
-        return negative_log
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+    def backwards(self, dvalues, y_true):
+        """
+        Run the derivative values backwards
+        Args:
+            dvalues: derivative values given
+            y_true: ground truth values
+        """
+        samples = len(dvalues)
+        labels = len(dvalues[0])
+
+        # discrete => one-hot encoded
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+
+        # Gradient calculation and normalize
+        self.dinputs = -y_true / dvalues
+        self.dinputs = self.dinputs / samples
 
 
-X, y = vertical_data(samples=100, classes=3)
+class Activation_Softmax_Loss_CategoricalCrossEntropy():
 
-# Input Layer - 2 inputs (x,y) and 3 outputs
-dense1 = Layer_Dense(2,3)
-activation1 = Activation_RelU()
+    def __init__(self):
+        """
+        Combined Softmax activation and cross-entropy because the backward step is WAY faster this way
+        Allows us to run the forward step of the other classes and define our own backwards
+        """
+        self.activation = Activation_Softmax()
+        self.loss = Loss_CategoricalCrossEntropy()
 
-# Layer 2 - 3 input and 3 output. Want to guess color (class) based on x,y values
-dense2 = Layer_Dense(3, 3)
-activation2 = Activation_Softmax()
+    # Forward pass
+    def forward(self, inputs, y_true):
+        """
+        Perform the forward softmax / loss calculation
+        Args:
+            inputs: array of inputs
+            y_true: array of ground truth values
+        """
 
-loss_function = Loss_CategoricalCrossEntropy()
+        # Do activation, calculate output, return
+        self.activation.forward(inputs)
+        self.output = self.activation.output
+        return self.loss.calculate(self.output, y_true)
 
-# Now we want to keep track of what we are using
-lowest_loss = 999999
-best_dense1_weights = dense1.weights.copy()
-best_dense1_biases = dense1.biases.copy()
-best_dense2_weights = dense2.weights.copy()
-best_dense2_biases = dense2.biases.copy()
+    def backward(self, dvalues, y_true):
+        """
+        Run the functions backwards which resultsi in y-hat - y as shown in the derivation section of the NNFS book
+        Args:
+            dvalues: derivative values to feed in
+            y_true: ground truth array
+        """
+        samples = len(dvalues)
+        # One-hot => descrete conversion. Will return array of where the argmax is located for each sample
+        #[[1,0,0],[0,0,1],[0,1,0]] => [0,2,1]
+        if len(y_true.shape)==2:
+            y_true = np.argmax(y_true, axis=1)
+
+        # I do not understand how this part is working. An example would be wonderful. This is taking advantage of
+        # one-hot encoding to make it work, but I don't know what "self.dinputs" looks like
+        self.dinputs = dvalues.copy()
+        self.dinputs[range(samples), y_true] -= 1
+        self.dinputs = self.dinputs / samples
 
 
-for iteration in range(10000):
 
-    dense1.weights += 0.05*np.random.randn(2,3)
-    dense1.biases += 0.05*np.random.randn(1,3)
-    dense2.weights += 0.05*np.random.randn(3,3)
-    dense2.biases += 0.05*np.random.randn(1,3)
+if __name__ == "__main__":
 
-    # Run layer1
-    dense1.forward(X)
-    activation1.forward(dense1.output)
-    # Run layer2
-    dense2.forward(activation1.output)
-    activation2.forward(dense2.output)
+    softmax_outputs = np.array([[0.7, 0.1, 0.2],
+                               [0.1, 0.5, 0.4],
+                               [0.02, 0.9, 0.08]])
 
-    # Calculate loss, accuracy
-    loss = loss_function.calculate(activation2.output, y)
-    predictions = np.argmax(activation2.output, axis=1)
-    accuracy = np.mean(predictions == y) # Works because y is one of (0,1,2)
+    class_targets = np.array([1,0,0])
 
-    if loss < lowest_loss:
-        # Update with new values
-        print(f"New values --> Iteration: {iteration}\tLoss: \
-        {loss}\tAccuracy: {accuracy}")
-        best_dense1_weights = dense1.weights.copy()
-        best_dense1_biases = dense1.biases.copy()
-        best_dense2_weights = dense2.weights.copy()
-        best_dense2_biases = dense2.biases.copy()
-        lowest_loss = loss
-        best_accuracy = accuracy
-        best_iteration = iteration
-
-    else:
-        # Reset values to best values
-        dense1.weights = best_dense1_weights
-        dense1.biases = best_dense1_biases
-        dense2.weights = best_dense2_weights
-        dense2.biases = best_dense2_biases
-
-print("FINAL RESULTS: ")
-print(f"New values --> Iteration: {best_iteration}\tLoss: \
-{lowest_loss}\tAccuracy: {best_accuracy}")
+    softmax_loss = Activation_Softmax_Loss_CategoricalCrossEntropy()
+    import pudb; pudb.set_trace()
+    softmax_loss.backward(softmax_outputs, class_targets)
+    dvalues1 = softmax_loss.dinputs
